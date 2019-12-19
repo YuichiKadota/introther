bfaf2c19c27fd23109272b193e6309d402a834ac
Pirokill <59017661+Pirokill@users.noreply.github.com>
2019-12-19 23:31:50 +0900

    力こそパワー

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import "fmt"
 func main(){
 	
 	c := add(1,3)
+	d := multiprication(2,4)
 
 	fmt.Println(c)
+	fmt.Println(d)
 
 }
 
@@ -14,4 +16,9 @@ func main(){
 func add(a,b int) int{
 
 	return a + b
+}
+
+func multiprication(a,b int) int{
+
+	return a * b
 }
